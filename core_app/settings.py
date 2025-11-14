f284784caa8ac6cec90154d0267d17c8ddca654d
Srinath Abburi <abburisrinath09@gmail.com>
2025-11-14 21:33:36 +0530

    Last commit on Nov-14 before pulling lastest code from main branch

--- core_app/settings.py
+++ core_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import environ, os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,6 +58,7 @@ TENANT_APPS = [
     'orders',
     'users',
     "themes"
+    'backups'
 ]
 
 INSTALLED_APPS = list(SHARED_APPS) + [ a for a in TENANT_APPS if a not in SHARED_APPS]
