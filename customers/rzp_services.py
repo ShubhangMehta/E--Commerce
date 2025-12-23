35dff80f0625b886e0f3c6afa68690e31345006c
bodakunta1 <bvenkatesh763@gmail.com>
2025-12-23 11:58:28 +0530

    added requirements.lock  file to code base

--- customers/rzp_services.py
+++ customers/rzp_services.py
@@ -76,7 +76,6 @@ def create_or_fetch_plan(plan):
     payment = Payment.objects.create(
         client=client,
         amount=amount,
-        method="UPI",                # or "CARD" or dynamic, your choice
         payment_plan=payment_plan,
         transaction_id="",           # will fill with Razorpay order_id
         status="unpaid",
