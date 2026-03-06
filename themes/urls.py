a7d7ef5e675ad713918e8d91272c4dddf8e3e798
bodakunta1 <bvenkatesh763@gmail.com>
2026-03-06 22:32:41 +0530

    fix: remove password reset confirmation template and related URL paths

--- themes/urls.py
+++ themes/urls.py
@@ -27,10 +27,6 @@ urlpatterns = [
 
     path("login/", c_views.login_view, name="login"),
     path("password_reset/", c_views.forgot_password_view, name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name="password_reset_done"),
-
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
-    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_complete'),
-
+    
 
 ]
