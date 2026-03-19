c1b489424fa830aa61b2c3c2e76bee040f1d5401
bodakunta1 <bvenkatesh763@gmail.com>
2026-03-19 13:23:03 +0530

    chore: remove initial migration files for accounts, backups, catalog, customers, notifications, orders, payments, and users models

--- core_app/settings.py
+++ core_app/settings.py
@@ -56,9 +56,7 @@ SHARED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    
-    
+    'rest_framework',    
 ]
 
 
