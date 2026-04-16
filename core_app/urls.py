e71e1f83a7b989416c065c6ed8bb3c3580101182
Onkar <shineonkar651@gmail.com>
2026-04-16 15:51:18 +0530

    16

--- core_app/urls.py
+++ core_app/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 #admin.autodiscover()
+app_name="users"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('accounts.urls')),
