125a2f10fc817da9e653f0b4c6759938bc292158
bodakunta1 <bvenkatesh763@gmail.com>
2026-04-16 17:07:34 +0530

    feat: simplify accounts/views.py
    - Removed commented-out legacy login logic and placeholder code.
    - Kept only the active `login_view` implementation that handles `next` URL extraction.

--- accounts/views.py
+++ accounts/views.py
@@ -11,56 +11,6 @@ from .models import LoginSession, TwoFactorCode
 from django.conf import settings
 from themes.views import _theme_path
 
-# Create your views here.
-
-# def login_view(request):
-#     next_url = request.GET.get('next') or request.POST.get('next') or '/' #Dont give admin page to next url, its will create the endless login loop
-
-#     # # Avoid sending users into admin/login loops
-#     # # (you can tune these rules to match your routing)
-#     # if next_url.startswith("/admin"):
-#     #     next_url = "/"
-
-#     template = "accounts/login.html" # default for public schema
-
-#     if getattr(request, "tenant", None) and request.tenant.schema_name != "public":
-#         template = _theme_path(request, "login.html")
-
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=username, password=password)
-
-#         if user is None:
-#             messages.error(request, 'Invalid username or password.')
-        
-#         else:
-#             #Non-Staff users: login directly
-#             if not user.is_staff:
-#                 login (request, user)
-#                 return redirect(next_url)
-
-#             ##Code for generating and sending 6-digits 2FA code for superuser
-#             code = TwoFactorCode.generate_code()
-#             TwoFactorCode.objects.create(user=user, code=code)
-#             send_mail(
-#                 'Your 2FA code',
-#                 f'Your one-time login code is: {code}',
-#                 settings.DEFAULT_FROM_EMAIL,
-#                 [user.email],
-#                 fail_silently=False,
-#             )
-#             request.session['pending_user'] = user.id
-#             request.session['pending_next'] = next_url
-#             request.session.modified = True
-
-#             print("Debug: pending_user set -> ", request.session['pending_user'])
-
-#             # Redirect to 2FA verification page
-#             return redirect('verify_2fa')
-        
-#     return render(request, template, {'next': next_url})
-
 
 def login_view(request):
     next_url = request.GET.get('next') or request.POST.get('next') or '/' #Dont give admin page to next url, its will create the endless login loop
